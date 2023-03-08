c5bac2fda84b695abec3deaf43fe73c6cc61fe6c
ItsMY_ <itsmyunderscore.sg@gmail.com>
2023-03-08 20:41:34 +0100

    addition of README.md

--- ItsMY_M/Constants.cs
+++ ItsMY_M/Constants.cs
@@ -3,7 +3,7 @@ namespace ItsMY_M;
 public class Constants
 {
     public const string con_str = "Server=localhost\\SQLEXPRESS;Database=Messenger;Trusted_Connection=True;";
-    public const bool debug = true;
+    public const bool debug = false;
     public static DatabaseConnectionManager DatabaseConnectionManager = new(con_str);
     public static readonly DateTime epoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 }
